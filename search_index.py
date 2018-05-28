f25b9ef8777931672b7fd2eb0b4c9af50190118d
zachlittle <zlittle@uci.edu>
2018-05-28 14:46:02 -0700

    Rewrote comment structure to make my thoughts more clear

--- search_index.py
+++ search_index.py
@@ -16,10 +16,25 @@ def search(search_query, database):
     ranked_list = []
     valid_records = _find_valid_documents(search_query, database)
 
-    for record in valid_records:
-        print(record)
+    #for term in search_query:
+    #    for record in valid_records:
+
+
+
 '''
 Basically now we need to sum the tf-idf scores for each term in the search query for each document
+according to https://piazza.com/class/jfk8ckl0nbujw?cid=517 "The "default" method would just be summing tf-idf scores 
+for each term in the search query for each document."
+
+ 
+I don't fully understand how to do this but these are my thoughts so far: 
+
+steps: 
+look at each term in the search query
+find the documents which contain most of the terms
+sum the tf-idf scores for each term contained in the document
+rank the documents by the total tf-idf and then create the return set
+
 search query: people get info
 
 results: 
@@ -29,20 +44,5 @@ results:
 (u'people', u"{'4/214': 1.6666666666666667, '13/481': 5.0, '13/480': 1.6666666666666667}")
 (u'bioinformatics', u"{'13/480': 10.0}")
 
-steps: 
-'''
 
-
-
-'''
-   for search_term, key, documents_and_tfidf in valid_records:
-        doc_dict = literal_eval(documents_and_tfidf.encode('UTF-8'))
-
-        summed_tfidf = sum(doc_dict.values())
-
-        print(summed_tfidf)
-
-        ranked_list.append((valid_records, summed_tfidf))
-
-    print ranked_list
 '''
